4aef6823b31c306d02d433a32e2ade52acd19580
Anna Dobrovidova <quilt.lob_0v@icloud.com>
2024-04-15 22:40:53 +0100

    add csv export

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+import { mkConfig, generateCsv, asString } from "export-to-csv";
+import { writeFile } from "node:fs";
+import { Buffer } from "node:buffer";
+
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
-const { chromium } = require("playwright");
+import { chromium } from "playwright";
+const csvConfig = mkConfig({ useKeysAsHeaders: true });
+
 
 async function saveHackerNewsArticles() {
   // launch browser
@@ -13,10 +19,25 @@ async function saveHackerNewsArticles() {
   await page.waitForSelector('#hnmain');
 
   // link = page.locator('.item-info-container ').get_by_role('link').get_attribute('href')
-  const articles = await page.locator('.titleline > a').all();
-  for (const article of articles) {
-    console.log(await article.getAttribute('href'));
+  const articleElements = await page.locator('.titleline > a').all();
+  const articles = []; 
+  for (const article of articleElements) {
+    articles.push(await article.getAttribute('href'));
   }
+
+  let exportArticles = articles.map((a, index) => { return { 'order': index, 'title': a, 'link': a } });
+
+  console.log(exportArticles);
+
+  const csv = generateCsv(csvConfig)(exportArticles);
+  const filename = `${csvConfig.filename}.csv`;
+  const csvBuffer = new Uint8Array(Buffer.from(asString(csv)));
+
+  // Write the csv file to disk
+  writeFile(filename, csvBuffer, (err) => {
+    if (err) throw err;
+    console.log("file saved: ", filename);
+  })
 
   // const articles = await page.$$eval('.storylink', (links) => {
   //   return links.slice(0, 10).map((link) => ({
