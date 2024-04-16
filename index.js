db86261fa5cf971fc90af3b4170872c93d792c66
Anna Dobrovidova <quilt.lob_0v@icloud.com>
2024-04-16 11:34:51 +0100

    remove comments

--- index.js
+++ index.js
@@ -16,22 +16,20 @@ async function saveHackerNewsArticles() {
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com");
-
   await page.waitForSelector('#hnmain');
 
-  // link = page.locator('.item-info-container ').get_by_role('link').get_attribute('href')
-  const articleElements = await page.locator('.titleline > a').all();
-  const articles = []; 
+  const articleElements = (await page.locator('.titleline > a').all()).slice(0, 10);
+  const articles = [];
+  let order = 0;
   for (const item of articleElements) {
     const article = { 
-      'order': 0, 
+      'order': order, 
       'title': await item.innerText(),
       'link': await item.getAttribute('href') 
     };
+    order++;
     articles.push(article);
   }
-
-  console.log(articles);
 
   const csv = generateCsv(csvConfig)(articles);
   const filename = `${csvConfig.filename}.csv`;
@@ -42,17 +40,6 @@ async function saveHackerNewsArticles() {
     if (err) throw err;
     console.log("file saved: ", filename);
   });
-
-  // const articles = await page.$$eval('.storylink', (links) => {
-  //   return links.slice(0, 10).map((link) => ({
-  //     title: link.innerText,
-  //     url: link.href
-  //   }));
-  // });
-
-  // articles.forEach((article, index) => {
-  //   console.log(`Article ${index + 1}: ${article.title} - ${article.url}`);
-  // });
 
   // Close the browser
   await page.close();
